189e8a85747b4407d6d5ac8b30bff14351fe2c1e
Diego Diniz <oitopatas.email@gmail.com>
2021-10-28 20:45:47 -0300

    Add the main file of the game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,9 @@ class alien_invasion:
         self.screen = pygame.display.set_mode(1200, 800)
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color in RGB.
+        self.bg_color = (230, 230, 230)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -18,6 +21,9 @@ class alien_invasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
