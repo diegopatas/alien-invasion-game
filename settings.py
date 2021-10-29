570c0bf82e1b3b9f902ad52bb3730c2ea68e91c7
Diego Diniz <oitopatas.email@gmail.com>
2021-10-28 21:04:35 -0300

    Add a settings class.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class settings:
+    """A class to store the settings for the game."""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
