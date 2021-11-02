5fabfbf88a6f4b27999f7a7b5d29c898d848a58e
Diego Diniz <oitopatas.email@gmail.com>
2021-11-02 00:13:41 -0300

    Add ship to the game and refactors methods.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,7 @@
 import sys
 import pygame
+from settings import settings
+from ship import ship
 
 class alien_invasion:
     """Overall class to manage game assets and behavior."""
@@ -7,26 +9,32 @@ class alien_invasion:
     def __init__(self):
         """Initialize the game and create game resources."""
         pygame.init()
+        self.settings = settings()
 
-        self.screen = pygame.display.set_mode(1200, 800)
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        # Set the game's background color in RGB.
-        self.bg_color = (230, 230, 230)
+        self.ship = ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # Redraw the screen during each pass through the loop.
-            self.screen.fill(self.bg_color)
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    def _update_screen(self):
+        """Update images on the screen and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = alien_invasion()
