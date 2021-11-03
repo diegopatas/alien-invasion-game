a1c87d6094fa62d7c45d91aa997f4e2608b09c2b
Diego Diniz <oitopatas.email@gmail.com>
2021-11-03 17:42:46 -0300

    Add a fullscreen mode.

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,10 @@ class alien_invasion:
         pygame.init()
         self.settings = settings()
 
+        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
