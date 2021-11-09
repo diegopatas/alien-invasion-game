2924a65836856cdbbac15799746264a544d3a47b
Diego Diniz <oitopatas.email@gmail.com>
2021-11-09 19:38:09 -0300

    Add bullet class to manage the ship's bullet behavior.

---
+++ bullet.py
@@ -0,0 +1,28 @@
+import pygame
+from pygame.sprite import Sprite
+
+class bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+
+    def __init__(self, ai_game):
+        """Create a bullet object at the ship's current position."""
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
+
+        # Create a bullet rect at (0,0) and then set correct position.
+        self.rect = pygame.Rect(0,0, self.settings.screen_width, self.settings.bullet_height) # we need to build the rectangle from scratch
+        self.rect.midtop = ai_game.ship.rect.midtop # needs to match the ship's midtop attribute
+
+        #Store the bullet's position as a decimal value.
+        self.y = float(self.rect.y)
+
+    def update_bullet(self):
+        """Move the bullet up the screen."""
+        self.y -= self.settings.bullet_speed # update the decimal position of the bullet
+        self.rect.y = self.y # update the rect position
+
+    def draw_bullet(self):
+        """Draw the bullet to the screen."""
+        pygame.draw.rect(self.screen, self.color, self.rect)
