075db60e1c4c52e5133c76b382da3657a7f52bae
Diego Diniz <oitopatas.email@gmail.com>
2021-11-10 19:52:26 -0300

    Add bullets settings used in bullet and alien_invasion class.

--- settings.py
+++ settings.py
@@ -4,7 +4,13 @@ class settings:
     def __init__(self):
         """Initialize the game's settings."""
         # Screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1000
+        self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        #Bullets settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 1
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
