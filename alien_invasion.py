c3b8f9a620230781876676b723fb0dbc326137b9
Diego Diniz <oitopatas.email@gmail.com>
2021-11-16 15:32:41 -0300

    Add movement to the alien's fleet.

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class alien_invasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -76,6 +77,10 @@ class alien_invasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
