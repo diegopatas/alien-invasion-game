e4df20c17d04d5db272430d04210be0cc3681a88
Diego Diniz <oitopatas.email@gmail.com>
2021-11-16 15:17:18 -0300

    Add a calculation to multiply rows of the fleet and we include it in drawing loop.

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,23 +80,32 @@ class alien_invasion:
     def _create_fleet(self):
         """Create the fleet of aliens."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2*alien_width)
         number_aliens_x = available_space_x//(2*alien_width)
 
-        for alien_number in range(number_aliens_x):
-            self._create_alien(alien_number)
+        # Determine the number of rows of aliens that fit on the screen.
+        ship_height = self.ship.rect.height
+        available_space_y = (self.settings.screen_height -
+                             (3*alien_height) - ship_height)
+        number_rows = available_space_y//(2*alien_height)
+        # _explanation
+        # here we calculate the available space to create multiple rows of the fleet.
 
-    def _create_alien(self, alien_number):
+        for row_number in range(number_rows):
+            for alien_number in range(number_aliens_x):
+                self._create_alien(alien_number, row_number)
+
+    def _create_alien(self, alien_number, row_number):
         """Create an alien and place it in the row."""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         alien.x = alien_width + 2 * alien_width * alien_number
         alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
         self.aliens.add(alien)
         # _explanation
-        # Here we refactor the code to organize the way we create an alien
-        # so we create a helper method
+        # We use a similar procedure to create multiple rows of the fleet
 
     def _update_screen(self):
         """Update images on the screen and flip to the new screen."""
