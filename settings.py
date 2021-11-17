ffa3ae22c738267efd89d12d488cb0dc95c5aece
Diego Diniz <oitopatas.email@gmail.com>
2021-11-17 17:32:55 -0300

    Add a game statistics class.

--- settings.py
+++ settings.py
@@ -8,6 +8,7 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        self.ship_limit = 3
         #Bullets settings
         self.bullet_speed = 1.0
         self.bullet_width = 1
